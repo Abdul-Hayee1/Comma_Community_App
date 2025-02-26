d2ebfa07182b3329879d0603ec5845d5966fe964
Umer Sarfraz <umer.sarfraz@xcellmobi.com>
2025-02-26 23:36:59 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Hayee\'s App',
+      title: 'Hayee App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
