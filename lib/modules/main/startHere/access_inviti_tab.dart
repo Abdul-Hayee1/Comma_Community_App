81fa18dedba1c68ef94653b6828a14240ffd5159
Amaz <amaz@xcellmobi.com>
2025-04-20 21:13:33 +0500

    access comma post

--- lib/modules/main/startHere/access_inviti_tab.dart
+++ lib/modules/main/startHere/access_inviti_tab.dart
@@ -5,21 +5,219 @@ class AccessInvitiTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: ColoredBox(
-        color: Color.fromARGB(255, 10, 39, 63),
+        color: const Color.fromARGB(255, 10, 39, 63),
         child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 12),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
+                  'John Doe commented on this 1d ago',
+                  style: TextStyle(color: Colors.grey[400], fontSize: 12),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const CircleAvatar(
+                          backgroundColor: Colors.blueGrey,
+                          child: Icon(
+                            Icons.shield,
+                            color: Colors.white,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                const Text(
+                                  'HA Support',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                const SizedBox(width: 4),
+                                Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[800],
+                                    shape: BoxShape.circle,
+                                  ),
+                                  padding: const EdgeInsets.all(4),
+                                  child: const Icon(
+                                    Icons.star,
+                                    color: Colors.amber,
+                                    size: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              'Host',
+                              style: TextStyle(
+                                color: Colors.grey[400],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 8),
+                    Container(
+                      padding: const EdgeInsets.only(
+                          top: 24, bottom: 4, left: 24, right: 24),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(Icons.arrow_drop_down, size: 32),
+                              SizedBox(width: 6),
+                              Text('COMMA'),
+                              SizedBox(width: 4),
+                              Icon(Icons.arrow_downward, size: 18),
+                            ],
+                          ),
+                          SizedBox(height: 12),
+                          ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: CircleAvatar(
+                              radius: 18,
+                              backgroundColor: Color.fromARGB(255, 5, 35, 60),
+                              child: Image(
+                                image: AssetImage(
+                                  'assets/logos/app_logo.png',
+                                ),
+                                width: 70,
+                                height: 70,
+                              ),
+                            ),
+                            title: Text('Comma Home Feed'),
+                          ),
+                          ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            leading: CircleAvatar(
+                              radius: 18,
+                              backgroundColor: Color.fromARGB(255, 5, 35, 60),
+                              child: Image(
+                                image: AssetImage(
+                                  'assets/logos/app_logo.png',
+                                ),
+                                width: 70,
+                                height: 70,
+                              ),
+                            ),
+                            title: Text('Comma On-Demand'),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    const Text(
+                      "Joined Comma and can't see the below in your left-hand navigation?",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      "Updated 2w ago",
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    SizedBox(
+                      width: 145,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.grey[800],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Row(
+                          children: [
+                            Image.asset(
+                              "assets/logos/app_logo.png",
+                              width: 30,
+                              height: 30,
+                            ),
+                            const Text(
+                              'Welcome!',
+                              style: TextStyle(
+                                color: Colors.white70,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Row(
+                      children: [
+                        Icon(
+                          Icons.more_vert,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        Spacer(),
+                        Text(
+                          '14',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 4),
+                        Icon(
+                          Icons.chat_bubble_outline,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          '1',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(width: 4),
+                        Icon(
+                          Icons.favorite_border,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              const Divider(
+                color: Colors.grey,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Text(
                   'You are all caught up!',
                   style: TextStyle(color: Colors.grey),
-                )
-              ],
-            ),
+                ),
+              )
+            ],
           ),
         ),
       ),
